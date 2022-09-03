ab19af1c0ad2b347a535d9e40d13dae722b11e89
Yutaka <y.tajika529@gmail.com>
2022-09-03 14:15:02 +0900

    implement ShellExecutor

--- Sources/ShellExecutor/ShellExecutor.swift
+++ Sources/ShellExecutor/ShellExecutor.swift
@@ -1,6 +1,35 @@
-public struct ShellExecutor {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+public struct ShellExecutor {
+    @discardableResult
+    public func execute(_ command: String,
+                        arguments: [String] = [],
+                        additionalEnvironment: [String: String] = [:]) throws -> String {
+        let script = "\(command) \(arguments.joined(separator: " "))"
+
+        let env = ProcessInfo.processInfo.environment
+                             .merging(additionalEnvironment, uniquingKeysWith: { $1 })
+        let task = Process()
+
+        task.launchPath = "/bin/sh"
+        task.arguments = ["-c", script]
+        task.environment = env
+
+        let outputPipe = Pipe()
+        let errorPipe = Pipe()
+        task.standardOutput = outputPipe
+        task.standardError = errorPipe
+        task.launch()
+        task.waitUntilExit()
+
+        let outputMessage = String(data: outputPipe.fileHandleForReading.readDataToEndOfFile(), encoding: .utf8)?.trimmingCharacters(in: .newlines)
+        let errorMessage = String(data: errorPipe.fileHandleForReading.readDataToEndOfFile(), encoding: .utf8)?.trimmingCharacters(in: .newlines)
+
+        let status = task.terminationStatus
+        if status == 0 {
+            return outputMessage!
+        } else {
+            throw ShellExecutorError(exitStatus: status, description: errorMessage!)
+        }
     }
 }
--- Tests/ShellExecutorTests/ShellExecutorTests.swift
+++ Tests/ShellExecutorTests/ShellExecutorTests.swift
@@ -2,10 +2,27 @@ import XCTest
 @testable import ShellExecutor
 
 final class ShellExecutorTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ShellExecutor().text, "Hello, World!")
+    private var shell: ShellExecutor!
+
+    override func setUp() {
+        super.setUp()
+
+        shell = ShellExecutor()
+    }
+
+    func testPWD() throws {
+        let result = try shell.execute("pwd")
+        XCTAssertEqual(result, FileManager.default.currentDirectoryPath)
+    }
+
+    func testAddEnv() throws {
+        let result = try shell.execute("echo",
+                                       arguments: ["$MYENV"],
+                                       additionalEnvironment: ["MYENV": "test"])
+        XCTAssertEqual(result, "test")
+    }
+
+    func testThrowsWhenInvalidCommandCalled() throws {
+        XCTAssertThrowsError(try shell.execute("taji-taji/swift-shell-executor/invalid-command"))
     }
 }
